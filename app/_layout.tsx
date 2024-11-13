113eda204d9f9c9d44ac3061ff766891ad8fffc1
Alpha-T30 <enamult300@gmail.com>
2024-11-13 12:53:53 +0600

    readme updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
+    // You have to wrap the main layout with the provider we created earlier i.e. SimpleProivder to get the preffered data anywhere in our app.  we can wrap with multiple providers
     <SimpleProvider>
       <Stack>
         <Stack.Screen name="index"></Stack.Screen>
--- app/login.tsx
+++ app/login.tsx
@@ -4,6 +4,7 @@ import SimpleContext from "@/contextproviders/simpleContext";
 import getData from "@/contextproviders/simpleContext";
 
 export default function Login() {
+  // We can get the data like that:
   const { userName, age, education } = getData();
   console.log(age);
 
